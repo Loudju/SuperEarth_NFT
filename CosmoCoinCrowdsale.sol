688e80c3cc816a26fac9c1a80d367f1ae9a76d06
Vicky Lee <103230949+vickylee22@users.noreply.github.com>
2022-09-16 19:41:43 -0500

    Update CosmoCoinCrowdsale.sol

--- CosmoCoinCrowdsale.sol
+++ CosmoCoinCrowdsale.sol
@@ -1,51 +1,66 @@
 pragma solidity ^0.5.0;
 
-import "./KaseiCoin.sol";
+import "./CosmoCoin.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
 
 
-// Have the KaseiCoinCrowdsale contract inherit the following OpenZeppelin:
+// Have the CosmoCoinCrowdsale contract inherit the following OpenZeppelin:
 // * Crowdsale
 // * MintedCrowdsale
-contract KaseiCoinCrowdsale { // UPDATE THE CONTRACT SIGNATURE TO ADD INHERITANCE
+contract CosmoCoinCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, RefundableCrowdsale { // UPDATE THE CONTRACT SIGNATURE TO ADD INHERITANCE
     
     // Provide parameters for all of the features of your crowdsale, such as the `rate`, `wallet` for fundraising, and `token`.
     constructor(
-        // YOUR CODE HERE!
-    ) public Crowdsale(rate, wallet, token) {
+        uint256 rate, // rate in TKNbits
+        address payable wallet, // sale beneficiary
+        CosmoCoin token, // the CosmoCoin itself that the CosmoCoinCrowdsale will work with
+        uint goal, // the crowdsale goal
+        uint open, // the crowdsale opening time
+        uint close // the crowdsale closing time
+    ) public 
+        Crowdsale(rate, wallet, token)
+        CappedCrowdsale(goal)
+        TimedCrowdsale(open, close)
+        RefundableCrowdsale(goal)
+    {
         // constructor can stay empty
     }
 }
 
-/*
-contract KaseiCoinCrowdsaleDeployer {
-    // Create an `address public` variable called `kasei_token_address`.
-    // YOUR CODE HERE!
-    // Create an `address public` variable called `kasei_crowdsale_address`.
-    // YOUR CODE HERE!
+contract CosmoCoinCrowdsaleDeployer {
+    // Create an `address public` variable called `cosmo_token_address`.
+     address public cosmo_token_address;
+    // Create an `address public` variable called `cosmo_crowdsale_address`.
+    address public cosmo_crowdsale_address;
 
     // Add the constructor.
     constructor(
-       // YOUR CODE HERE!
+        string memory name,
+        string memory symbol,
+        address payable wallet,
+        uint initial_supply,
+        uint goal
     ) public {
-        // Create a new instance of the KaseiCoin contract.
-        // YOUR CODE HERE!
+        // Create a new instance of the CosmoCoin contract.
+        CosmoCoin token = new CosmoCoin(name, symbol, initial_supply);
         
-        // Assign the token contract’s address to the `kasei_token_address` variable.
-        // YOUR CODE HERE!
+        // Assign the token contract’s address to the `cosmo_token_address` variable.
+        cosmo_token_address = address(token);
 
-        // Create a new instance of the `KaseiCoinCrowdsale` contract
-        // YOUR CODE HERE!
+        // Create a new instance of the `CosmoCoinCrowdsale` contract
+        CosmoCoinCrowdsale cosmo_crowdsale = new CosmoCoinCrowdsale (1, wallet, token, goal, now, now + 24 weeks);
             
-        // Aassign the `KaseiCoinCrowdsale` contract’s address to the `kasei_crowdsale_address` variable.
-        // YOUR CODE HERE!
+        // Aassign the `CosmoCoinCrowdsale` contract’s address to the `cosmo_crowdsale_address` variable.
+        cosmo_crowdsale_address = address(cosmo_crowdsale);
 
-        // Set the `KaseiCoinCrowdsale` contract as a minter
-        // YOUR CODE HERE!
+        // Set the `CosmoCoinCrowdsale` contract as a minter
+        token.addMinter(cosmo_crowdsale_address);
         
-        // Have the `KaseiCoinCrowdsaleDeployer` renounce its minter role.
-        // YOUR CODE HERE!
+        // Have the `CosmoCoinCrowdsaleDeployer` renounce its minter role.
+        token.renounceMinter();
     }
 }
-*/
