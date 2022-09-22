aa06a30a2ee59b569c14f455348fc56671c29b86
Vicky Lee <103230949+vickylee22@users.noreply.github.com>
2022-09-22 11:16:54 -0500

    Update StarRegistry.sol

--- contracts/StarRegistry.sol
+++ contracts/StarRegistry.sol
@@ -2,23 +2,21 @@ pragma solidity ^0.5.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
 
-contract ArtRegistry is ERC721Full {
-    constructor() public ERC721Full("ArtRegistryToken", "ART") {}
+contract StarRegistry is ERC721Full {
+    constructor() public ERC721Full("StarRegistryToken", "STAR") {}
 
-    struct Artwork {
+    struct Starland {
         string name;
-        string artist;
         uint256 appraisalValue;
     }
 
-    mapping(uint256 => Artwork) public artCollection;
+    mapping(uint256 => Starland) public starlandCollection;
 
     event Appraisal(uint256 tokenId, uint256 appraisalValue, string reportURI);
 
-    function registerArtwork(
+    function registerStarland(
         address owner,
         string memory name,
-        string memory artist,
         uint256 initialAppraisalValue,
         string memory tokenURI
     ) public returns (uint256) {
@@ -27,7 +25,7 @@ contract ArtRegistry is ERC721Full {
         _mint(owner, tokenId);
         _setTokenURI(tokenId, tokenURI);
 
-        artCollection[tokenId] = Artwork(name, artist, initialAppraisalValue);
+        starlandCollection[tokenId] = Starland(name, initialAppraisalValue);
 
         return tokenId;
     }
@@ -37,10 +35,10 @@ contract ArtRegistry is ERC721Full {
         uint256 newAppraisalValue,
         string memory reportURI
     ) public returns (uint256) {
-        artCollection[tokenId].appraisalValue = newAppraisalValue;
+        starlandCollection[tokenId].appraisalValue = newAppraisalValue;
 
         emit Appraisal(tokenId, newAppraisalValue, reportURI);
 
-        return artCollection[tokenId].appraisalValue;
+        return starlandCollection[tokenId].appraisalValue;
     }
 }
